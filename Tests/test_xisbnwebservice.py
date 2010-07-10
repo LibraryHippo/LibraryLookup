338e4305df2ff8e1b87e8f5b07b925582339513c
Blair Conrad <blair_conrad@alumni.uwaterloo.ca>
2010-07-09 23:02:15 -0400

    stripping to13, to10 response and using app engine for ad hoc test

--- Tests/test_xisbnwebservice.py
+++ Tests/test_xisbnwebservice.py
@@ -27,21 +27,24 @@ def test_to13__10digits__makes_good_request():
     assert 'http://xisbn.worldcat.org/webservices/xid/isbn/0596002815?method=to13&format=csv' == opener.last_request['url']
 
 def test_to13__10digits__finds_good_alternate():
-    opener = MyOpener('9780596002817')
+    opener = MyOpener('9780596002817\r\n')
     x = xisbnwebservice.XisbnWebService(opener)
-    assert '9780596002817' == x.to13('0596002815')
+    result = x.to13('0596002815')
+    assert '9780596002817' == result
 
 def test_to10__13digits__makes_good_request():
     opener = MyOpener('')
     x = xisbnwebservice.XisbnWebService(opener)
     x.to10('0596002815')
 
-    assert 'http://xisbn.worldcat.org/webservices/xid/isbn/0596002815?method=to10&format=csv' == opener.last_request['url']
+    last_url = opener.last_request['url']
+    assert 'http://xisbn.worldcat.org/webservices/xid/isbn/0596002815?method=to10&format=csv' == last_url
 
 def test_to10__13digits__finds_good_alternate():
-    opener = MyOpener('0596002815')
+    opener = MyOpener('0596002815\r\n')
     x = xisbnwebservice.XisbnWebService(opener)
-    assert '0596002815' == x.to10('9780596002817')
+    result = x.to10('9780596002817')
+    assert '0596002815' == result
 
 
     
