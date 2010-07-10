ce4deeab1c51bdffca096c0cd046fb08482e1a68
Blair Conrad <blair_conrad@alumni.uwaterloo.ca>
2010-07-09 22:59:36 -0400

    returning first match from library

--- App/catalogue.py
+++ App/catalogue.py
@@ -13,16 +13,14 @@ class Catalogue:
             item = library.find_item(isbn)
             if item:
                 items.append(item)
-
-            other_editions = self.xisbn.get_editions(isbn)
-            for edition in other_editions:
-                if edition == isbn:
-                    continue
-                logging.debug('looking for ' + edition)
-                item = library.find_item(edition)
-                if item:
-                    items.append(item)
+            else:
+                other_editions = self.xisbn.get_editions(isbn)
+                for edition in other_editions:
+                    if edition == isbn:
+                        continue
+                    logging.debug('looking for ' + edition)
+                    item = library.find_item(edition)
+                    if item:
+                        items.append(item)
+                        break
         return items
-
-
-        
