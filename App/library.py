d31ad6664990ea812731a79fd2855fcbc00024f7
Blair Conrad <blair_conrad@alumni.uwaterloo.ca>
2010-08-07 09:06:10 -0400

    adding common library base

---
+++ App/library.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+class LibraryBase:
+    def __repr__(self):
+        return self.id
+
+    def __str__(self):
+        return self.name
+    
--- App/rwl.py
+++ App/rwl.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+import library
+
 def searchUrl(isbn):
     return 'http://www.regionofwaterloo.canlib.ca/uhtbin/cgisirsi/uOtJAQjVct/HEADQUARTR/x/5/0/?searchdata1=%(isbn)s' % vars()
 
-class Library:
+class Library(library.LibraryBase):
     def __init__(self, opener):
+        self.id = 'rwl'
         self.name = 'Region of Waterloo Library'
         self.opener = opener
 
