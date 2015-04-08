24629262ac438eb1d6dc47c4c65958f950eac47c
Blair Conrad <blair@blairconrad.com>
2015-04-08 07:25:48 -0400

    improving developer tools, to match LibraryHippo's

--- dev.bat
+++ dev.bat
@@ -1,2 +1,2 @@
-start "LL dev server" /D%~p0\App cmd /C ..\..\go.bat .
-start "LL working dir" /D%~p0 cmd
+start "LL dev server" /D%~p0 cmd /C powershell -NoExit .\serve.bat
+start "LL working dir" /D%~p0 cmd /C powershell -NoExit
